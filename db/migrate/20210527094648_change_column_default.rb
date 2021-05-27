2cc725d80e077e8d40fe6e67777deb504fb55000
Marina <rakastettu13@gmail.com>
2021-05-27 10:30:33 +0000

    Solving task 2

---
+++ db/migrate/20210527094648_change_column_default.rb
@@ -0,0 +1,7 @@
+class ChangeColumnDefault < ActiveRecord::Migration[6.1]
+  def change
+    change_column_default :tests, :level, from: nil, to: 0
+
+    change_column_default :answers, :correct, from: nil, to: false
+  end
+end
