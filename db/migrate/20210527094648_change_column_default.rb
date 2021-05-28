77a28ee0bb67dfa12712169b3c351a3f6a3187ea
Marina <rakastettu13@gmail.com>
2021-05-28 16:00:39 +0000

    changes in models and migrations

--- db/migrate/20210527094648_change_column_default.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnDefault < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :tests, :level, from: nil, to: 0
-
-    change_column_default :answers, :correct, from: nil, to: false
-  end
-end
