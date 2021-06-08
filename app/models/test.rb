8dc031e4adcbdbb0a6971bc63ddf1eea1562a270
Marina <rakastettu13@gmail.com>
2021-06-08 17:44:41 +0000

    added :dependent option and refactoring

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,16 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
-  has_many :questions
+
+  has_many :questions, dependent: :destroy
   has_many :answers, through: :questions
-  has_many :test_takers
+  has_many :test_takers, dependent: :destroy
   has_many :users, through: :test_takers
 
   def self.title_desc(category)
-    joins('JOIN categories ON categories.id = category_id')
-      .where('categories.title = ?', category).order(title: :desc).pluck(:title)
+    joins(:category)
+      .where(categories: { title: category })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  has_many :author_tests, class_name: 'Test', inverse_of: 'author', foreign_key: 'author_id'
-  has_many :test_takers
+  has_many :author_tests, class_name: 'Test', inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
+  has_many :test_takers, dependent: :destroy
   has_many :tests, through: :test_takers
+
   def find_tests(level)
-    Test.joins('JOIN test_takers ON test_id = tests.id').where('level = ? AND test_takers.user_id = ?', level, id)
+    Test.joins(:test_takers)
+        .where({ level: level, test_takers: { user: self } })
   end
 end
