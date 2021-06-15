b3b9b971a695eefe536c714cf581f0c114527d2f
Marina <rakastettu13@gmail.com>
2021-06-15 20:18:03 +0000

    added methods to questions_controller (index, show, create, destroy, ...)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,44 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render inline: '<% @test.questions.find_each do |question| %><p><%= question.body %></p><% end %>'
+  end
+
+  def show
+    render inline: '<%= @question.body %>'
+  end
+
+  def new; end
+
+  def create
+    question = @test.questions.create(question_params)
+    render plain: question.inspect
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to test_questions_path(@question.test_id)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Вопрос не найден'
+  end
 end
