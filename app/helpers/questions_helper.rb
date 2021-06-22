faf39e2228e283e6837c3b6a3c49a539cbaed46e
Marina <rakastettu13@gmail.com>
2021-06-22 08:46:20 +0300

    added footer and navigation

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_header(test)
+    action = if params[:action] == 'new'
+               'Create new'
+             else
+               params[:action].capitalize
+             end
+    "#{action} #{test.title} question"
+  end
 end
