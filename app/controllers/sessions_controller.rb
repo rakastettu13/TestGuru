a646aefcff1ed179bc3a69b5226532daf4a12cdf
Marina <rakastettu13@gmail.com>
2021-07-01 08:32:22 +0300

    destroy and reditect_back for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def new; end
 
   def create
@@ -6,10 +8,21 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tests_path
+      redirect_back_after_authenticate
     else
       flash.now[:alert] = 'Неверный пароль/email'
       render :new
     end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def redirect_back_after_authenticate
+    redirect_to cookies[:return_to_url] || root_path
   end
 end
