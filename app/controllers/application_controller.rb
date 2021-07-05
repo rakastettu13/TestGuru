5b058558b8e4205ac83629b9c91ddd13223267fe
Marina <rakastettu13@gmail.com>
2021-07-05 12:35:15 +0300

    added redirect to admin/tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      '/admin/tests#index'
+    else
+      root_path
+    end
+  end
 end
