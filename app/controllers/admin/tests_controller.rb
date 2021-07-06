b0fd42ea71aa4451c521cddf43150026003e2a2c
Marina <rakastettu13@gmail.com>
2021-07-07 00:02:03 +0300

    tests_controller refactoring

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -12,8 +12,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = Test.new(test_params)
-    @test.author = current_user
+    @test = current_user.tests.build(test_params)
 
     if @test.save
       redirect_to admin_test_path(@test)
