ed3f2c923ae8d9aa8253a54ac12f26e467b8797f
Marina <rakastettu13@gmail.com>
2021-07-07 00:03:40 +0300

    create action was added, set_flash_message! method was deleted

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,13 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in, username: resource.name)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -24,9 +28,4 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-
-  def set_flash_message!(key, kind, options = {})
-    options[:username] = resource.name if resource
-    set_flash_message(key, kind, options) if is_flashing_format?
-  end
 end
