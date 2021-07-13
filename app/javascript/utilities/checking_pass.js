f0fcbf24c44e8ee7081009d129665396200ddbe3
Marina <rakastettu13@gmail.com>
2021-07-13 15:22:35 +0300

    turbolinks:load event was removed from cheking_pass

--- app/javascript/utilities/checking_pass.js
+++ app/javascript/utilities/checking_pass.js
@@ -1,20 +1,18 @@
-document.addEventListener("turbolinks:load", function() {
-  let form = document.forms[0];
-    form.addEventListener("input", function() {
-      let value1 = form.user_password.value;
-      let value2 = form.user_password_confirmation.value;
-      
-      if (value2 === "") {
-        hide(form, "check");
-        hide(form, "x");
-      } else if (value1 === value2 ){
-        hide(form, "x");
-        show(form, "check");
-      } else {
-        hide(form, "check");
-        show(form, "x");
-      }
-    });
+let form = document.forms[0];
+  form.addEventListener("input", function() {
+    let value1 = form.user_password.value;
+    let value2 = form.user_password_confirmation.value;
+
+    if (value2 === "") {
+      hide(form, "check");
+      hide(form, "x");
+    } else if (value1 === value2 ){
+      hide(form, "x");
+      show(form, "check");
+    } else {
+      hide(form, "check");
+      show(form, "x");
+    }
 });
 
 function hide(form, icon) {
